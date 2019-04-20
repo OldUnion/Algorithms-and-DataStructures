be8adf45f7fc2bb165c724c2fee2bb8c32d8b66d
Dmitriy Evtushenko <oldunion1995@gmail.com>
2019-04-20 23:46:43 +0300

    add bubble sort implementation

---
+++ Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Sorting
+{
+    public static class BubbleSort
+    {
+        public static void Sort<T>(IList<T> array, IComparer<T> comparer = null)
+        {
+            var sortingComparer = comparer ?? Comparer<T>.Default;
+            for (int i = 0; i < array.Count(); i++)
+            {
+                for (int j = 0; j < array.Count() - i - 1; j++)
+                {
+                    if (sortingComparer.Compare(array[j], array[j + 1]) > 0)
+                    {
+                        array.Swap(j, j + 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestProject/AlgorithmsTests/BubbleSortTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Algorithms.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.AlgorithmsTests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            var sortingCollection = new List<int> { 5, 5, 6, 3, 2, 99, 3, -5, 33, 23, 435, 2, 23, 1 };
+            var expectedResult = sortingCollection.ToList();
+            expectedResult.Sort();
+
+            BubbleSort.Sort(sortingCollection);
+
+            CollectionAssert.AreEqual(sortingCollection, expectedResult);
+        }
+    }
+}
